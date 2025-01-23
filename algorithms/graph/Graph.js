1a77b4e5ddbfb914c8ff683185ef731b3db90b0e
Alex Abetov <abetovalex93@gmail.com>
2025-01-24 00:32:13 +0300

    Graph возвращает значение количества рёбер и вершин

--- algorithms/graph/Graph.js
+++ algorithms/graph/Graph.js
@@ -1,5 +1,12 @@
 class Graph{
-
+    #edges = []
+    #vercels = []
+    getEdgeCount(){
+        return this.#edges.length
+    }
+    getVercelCount(){
+        return this.#vercels.length
+    }
 }
 
 module.exports = Graph
