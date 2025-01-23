ad323514f91770266ac21c9f16ac1644461f72e6
Alex Abetov <abetovalex93@gmail.com>
2025-01-24 00:44:53 +0300

    Graph умеет создавать связи между точками в виде ребёр

--- tests/graph/graph.test.js
+++ tests/graph/graph.test.js
@@ -15,4 +15,26 @@ describe('Graph является совокупностью Vercel и Edge', () 
         graph.addVercel(v1)
         expect(graph.getVercelCount()).toBe(1)
     })
+    test('Граф может создавать связи в виде рёбер', () => {
+        const v1 = new Vercel('Moscow')
+        const v2 = new Vercel('Tambov')
+        graph
+        .addVercel(v1)
+        .addVercel(v2)
+        .createEdge(v1, v2)
+        expect(graph.getVercelCount()).toBe(2)
+        expect(graph.getEdgeCount()).toBe(1)
+    })
+    test('Граф выдает ошибку, если передать связь несуществующей вершины', () => {
+        const v1 = new Vercel('Moscow')
+        const v2 = new Vercel('Tambov')
+        graph
+        .addVercel(v1)
+        try{
+            graph.createEdge(v1, v2)
+        }
+        catch(e){
+            expect(e).toBeDefined()
+        }
+    })
 })
