caba1c5bded622f7cdf1f7fae436bcdf442bd800
AlexAbS <abetovalex93@gmail.com>
2025-02-09 03:23:09 +0300

    Добавлены отказоустойчивые тесты

--- algorithms/sort/select_sort.js
+++ algorithms/sort/select_sort.js
@@ -5,11 +5,21 @@ class SelectSort{
     dictionary = {}
     max = -Infinity
     sort(arr){
+        this.#validate(arr)
         this.defineMax(arr)
     }
     defineMax(arr){
         this.max = Math.max.apply(this, arr)
         return this.max
+    }
+    #validate(arr){
+        const requiredType = 'number'
+        if (!arr.every(n => typeof n === requiredType)){
+            throw new Error('Все элементы массива должны быть числами')
+        }
+        if(!arr.every(n => n >= 0)){
+            throw new Error('Все числа должны быть больше нуля')
+        }
     }
 }
 
