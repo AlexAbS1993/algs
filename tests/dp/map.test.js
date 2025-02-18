a1b26460ab493589eea878a2eae73e9a268446ce
AlexAbS <abetovalex93@gmail.com>
2025-02-18 20:21:15 +0300

    Сущность map завершена
    Добавлен метод получения карты и несколько статических методов создания
    особых видов карт.
    
    Проведены тесты, в том числе отказоустойчивые

--- tests/dp/map.test.js
+++ tests/dp/map.test.js
@@ -1,5 +1,5 @@
 const MapForTurtle = require("../../algorithms/dp/Turtle/map")
-
+const fs = require('fs')
 describe('Карта создаётся для тестирования алгоритма. Внутрь карты можно было бы включать особые объекты, \n' + 
     'но в этом случае это будут обычные js-объекты с какими-то данными (value)', () => {
         let map = new MapForTurtle(3, 3)
@@ -14,6 +14,54 @@ describe('Карта создаётся для тестирования алго
         })
         test('map может предоставлять данные об элементе по методу point', () => {
             expect(map.point(1, 1)).toEqual({value: null})
+        })
+        test('Сущность может использовать статические методы для создания карты по уже подготовленному массиву', () => {
+            let map = MapForTurtle.createMapFromArray([[1, 5], [2, 5], [-10, 0]])
+            expect(map.point(1, 2).value).toBe(5)
+        })
+        test('Сущность использует статический метод для создания карты с рандомными данными', () => {
+            let map = MapForTurtle.createRandomMap(3, 3)
+            let writeStream = fs.createWriteStream(__dirname + '/snapshots' + '/map.json')
+            writeStream.write(JSON.stringify(map.getMap()))
+            expect(typeof map.point(1, 1).value).toBe('number')
+        })
+        describe('Отказоустойчивость при неправильных аргументах работает выбрасыванием исключения', () => {
+            describe('Система выдаёт ошибку, если переданы неверные параметры в конструктор', () => {
+                test('Ошибка при передаче отрицательного значения', () => {
+                    try{
+                        new MapForTurtle(-1, 1)
+                    }
+                    catch(e){
+                        expect(e.message).toMatch('положительным')
+                    }
+                })
+                test('Ошибка при отсутствии передаваемого значения', () => {
+                    try{
+                       new MapForTurtle(4)
+                    }
+                    catch(e){
+                        expect(e.message).toMatch('аргументов')
+                    }
+                })
+                test('Ошибка при неправильном типе передаваемых данных', () => {
+                    try{
+                        new MapForTurtle(4, true)
+                    }
+                    catch(e){
+                        expect(e.message).toMatch('числом')
+                    }
+                })
+            })
+            describe('Выдаются ошибки при передаче неверных данных в методы работы с map', () => {
+                test('Выдается ошибка, если пользователь пытается получить данные, которых не существует', () => {
+                    try{
+                        map.point(10, 10)
+                    }
+                    catch(e){
+                        expect(e.message).toMatch('не существует')
+                    }
+                })
+            })
         })
     }
 )
