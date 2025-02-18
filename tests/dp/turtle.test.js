19d9059d63c4d0746dbb40e910fbe983fe538476
AlexAbS <abetovalex93@gmail.com>
2025-02-19 00:02:39 +0300

    В сущности TurtleV1 добавлен алгоритм для просчёта количества путей от старта до финиша
    Проведены тесты

--- tests/dp/turtle.test.js
+++ tests/dp/turtle.test.js
@@ -1,20 +1,20 @@
 const TurtleV1 = require("../../algorithms/dp/Turtle/_turtle")
+const MapForTurtle = require("../../algorithms/dp/Turtle/map")
 
 describe('Задачка с черепашкой находит количество возможных путей и самый оптимиальный путь', () => {
-    let turtle = new TurtleV1()
-    test('', () => {
-
+    let mini_map = MapForTurtle.createRandomMap(3, 3)
+    let middle_map =  MapForTurtle.createRandomMap(6, 6)
+    let large_map = MapForTurtle.createRandomMap(10, 10)
+    test('Задача может выдавать верное количество путей для любого поля', () => {
+        let turtle = new TurtleV1(mini_map)
+        let turtle_max = new TurtleV1(large_map)
+        let turtle_middle = new TurtleV1(middle_map)
+        turtle.calculateCountOfWaysFromStartToFinal()
+        turtle_max.calculateCountOfWaysFromStartToFinal()
+        turtle_middle.calculateCountOfWaysFromStartToFinal()
+        expect(turtle.getResult(turtle.operations.count_ways)).toEqual({count:6})
+        expect(turtle.getResult(turtle.operations.count_ways)).toEqual({count:48620})
+        expect(turtle.getResult(turtle.operations.count_ways)).toEqual({count:252})
     })
-    test('', () => {
-
-    })
-    test('', () => {
-
-    })
-    test('', () => {
-
-    })
-    test('', () => {
-
-    })
+   
 })
